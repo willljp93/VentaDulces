d5ff64acd3a8d5a8aff6230e0fc1937188d4057c
William <47790199+willljp93@users.noreply.github.com>
2023-03-30 20:01:25 -0400

    Update BarraBusquedaComponents.vue

--- frontend/src/components/catproduct/BarraBusquedaComponents.vue
+++ frontend/src/components/catproduct/BarraBusquedaComponents.vue
@@ -1,52 +1,44 @@
 <template>
-    <div class="col-8 q-pa-lg flex-center">
-      <q-input
-        dense
-        standout="bg-primary text-white"
-        rounded
-        type="search"
-        placeholder="Buscar"
-        v-model="search"
-        input-class="text-right"
-        class="q-ml-xs"
-      >
-        <template v-slot:append>
-          <q-icon v-if="search === ''" name="search" />
-          <q-icon
-            v-else
-            name="clear"
-            class="cursor-pointer"
-            @click="search = ''"
-          />
-        </template>
-      </q-input>
-    </div>
+  <div class="col-8 q-pa-lg flex-center">
+    <q-input
+      dense
+      standout="bg-primary text-white"
+      rounded
+      type="search"
+      placeholder="Buscar"
+      v-model="search"
+      input-class="text-right"
+      class="q-ml-xs"
+    >
+      <template v-slot:append>
+        <q-icon v-if="search === ''" name="search" />
+        <q-icon
+          v-else
+          name="clear"
+          class="cursor-pointer"
+          @click="search = ''"
+        />
+      </template>
+    </q-input>
+  </div>
 </template>
 
 <script>
-import { ref, computed } from "vue";
-export default {
-  name: "ListCardProductComponents",
-  props: {
-    maxPrice: {
-      type: Number,
-      default: Infinity,
-    },
-  },
-  setup(props) {
-    const search = ref("");
+import { ref } from "vue";
 
-    const filteredItems = computed(() => {
-      return props.items.filter(
-        (item) =>
-          item.title.toLowerCase().includes(search.value.toLowerCase()) &&
-          item.price <= props.maxPrice
-      );
-    });
+<script setup>
+import { ref, watch } from "vue";
 
-    return {
-      search,
-        };
+const props = defineProps({
+  value: {
+    type: String,
+    default: "",
   },
-};
+});
+
+const emit = defineEmits(["update:value"]);
+
+const search = ref(props.value);
+
+watch(search, (newValue) => emit("update:value", newValue));
 </script>
